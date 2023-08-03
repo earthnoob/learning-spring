751835c536cbbf729915643f65099db966894a8a
Lam Le <lamlnl@ctnpsolutions.com>
2023-08-03 14:27:58 +0700

    Lesson 7. Spring Boot - Create a REST Controller

---
+++ src/main/java/com/nbrth/learningspring/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package com.nbrth.learningspring.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // Expose "/" that returns "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+}
