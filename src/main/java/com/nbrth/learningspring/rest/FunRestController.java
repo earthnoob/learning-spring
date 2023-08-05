ac34ac65da08544ced0652c059a4ed17fbd29223
Lam Le <lamlnl@ctnpsolutions.com>
2023-08-05 13:40:59 +0700

    Lessons 30.Configuring the Spring Boot Server - Coding

--- src/main/java/com/nbrth/learningspring/rest/FunRestController.java
+++ src/main/java/com/nbrth/learningspring/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.nbrth.learningspring.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
 
     // Expose "/" that returns "Hello World"
     @GetMapping("/")
