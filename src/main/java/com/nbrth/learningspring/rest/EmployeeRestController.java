d7e5f7a07e09cfa1ec7dae0c825dae593c11b44e
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-26 16:58:23 +0700

    Cherry picked from section-4

---
+++ src/main/java/com/nbrth/learningspring/rest/EmployeeRestController.java
@@ -0,0 +1,36 @@
+package com.nbrth.learningspring.rest;
+
+import com.nbrth.learningspring.entity.Employee;
+import com.nbrth.learningspring.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private final EmployeeService employeeService;
+
+    // Inject employee service
+    @Autowired
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // Expose "/employee" and return a list of employees
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable(name = "employeeId") int id) {
+        return employeeService.findById(id);
+    }
+
+}
