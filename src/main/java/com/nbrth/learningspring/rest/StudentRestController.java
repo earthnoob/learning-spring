d0ce5042b837dcae65fe5b86b59a479b0935c600
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-26 12:01:17 +0700

    Lessons 100 + 101 + 102. Spring Boot REST Path Variables - Coding

--- src/main/java/com/nbrth/learningspring/rest/StudentRestController.java
+++ src/main/java/com/nbrth/learningspring/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.nbrth.learningspring.rest;
 
 import com.nbrth.learningspring.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +14,30 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>() {
+    private List<Student> theStudents;
+
+    // Define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>() {
             {
                 add(new Student("John", "Doe"));
                 add(new Student("Jane", "Doe"));
                 add(new Student("Bella", "Jones"));
             }
         };
+    }
 
+    // Define endpoint for "/students" - Return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    // Define endpoint for "/students/{studentId}" - Return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
     }
 
 }
