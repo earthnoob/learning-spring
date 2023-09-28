d516bde1dd0d997b9305cf3b9a5eebe97875fc8c
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-28 13:54:08 +0700

    Lesson 128. Spring Boot REST: Delete Employee - Coding

--- src/main/java/com/nbrth/learningspring/rest/EmployeeRestController.java
+++ src/main/java/com/nbrth/learningspring/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
@@ -41,6 +42,17 @@ public class EmployeeRestController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
         return employeeService.save(theEmployee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable("employeeId") int id) {
+        if (Objects.isNull(employeeService.findById(id))) {
+            throw new RuntimeException("Employee id not found - " + id);
+        }
+
+        employeeService.deleteById(id);
+
+        return "Deleted employee id - " + id;
     }
 
 }
