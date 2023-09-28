6116b7710998f17b80a2ba6bb36f0dce99a334fe
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-28 13:46:29 +0700

    Lesson 127. Spring Boot REST: Update Employee - Coding

--- src/main/java/com/nbrth/learningspring/rest/EmployeeRestController.java
+++ src/main/java/com/nbrth/learningspring/rest/EmployeeRestController.java
@@ -2,7 +2,6 @@ package com.nbrth.learningspring.rest;
 
 import com.nbrth.learningspring.entity.Employee;
 import com.nbrth.learningspring.service.EmployeeService;
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +35,11 @@ public class EmployeeRestController {
     public Employee addEmployee(@RequestBody Employee theEmployee) {
         // Set id to 0 in case users pass in id
         theEmployee.setId(0);
+        return employeeService.save(theEmployee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
         return employeeService.save(theEmployee);
     }
 
