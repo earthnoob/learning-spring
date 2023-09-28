979470cad05c3b686f5704cb4a55a97cef649da6
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-28 15:55:24 +0700

    Cherry picked from section-4

---
+++ src/main/java/com/nbrth/learningspring/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.nbrth.learningspring.dao;
+
+import com.nbrth.learningspring.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
