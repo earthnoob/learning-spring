b6a67d2469cf30f65096f400eda63ccb13879491
Lam Le <lamlnl@ctnpsolutions.com>
2023-09-28 15:55:24 +0700

    Lessons 135 + 136. Spring Boot REST: Spring Data REST Configs and Sorting

--- src/main/java/com/nbrth/learningspring/dao/EmployeeRepository.java
+++ src/main/java/com/nbrth/learningspring/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.nbrth.learningspring.dao;
 
 import com.nbrth.learningspring.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
