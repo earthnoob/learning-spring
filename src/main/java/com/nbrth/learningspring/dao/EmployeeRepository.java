cf01ea95f94e263dffc7da381c1b29dcf0ac6da1
Lam Le <lamlnl@ctnpsolutions.com>
2023-10-16 16:38:10 +0700

    Lessons 142 - 145. Spring Boot REST API Security - Restrict URLs based on Roles

--- src/main/java/com/nbrth/learningspring/dao/EmployeeRepository.java
+++ src/main/java/com/nbrth/learningspring/dao/EmployeeRepository.java
@@ -4,6 +4,6 @@ import com.nbrth.learningspring.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+@RepositoryRestResource()
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
